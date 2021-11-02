a974947d82e8c026292d3e7679431b01d790fbbc
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-11-02 21:43:26 +0200

    chart done

--- src/components/admin/Chart.jsx
+++ src/components/admin/Chart.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
-
+import { Doughnut } from 'react-chartjs-2';
+import styles from '../../styles/chart.module.css';
+const data = {
+  labels: ['Serial Complaints', 'Personal Complaints', 'Delivery Complaints', 'Quality Complaints'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5],
+      backgroundColor: ['#62a403', '#ff7800', '#055052', '#C400FF'],
+      borderColor: ['#62a403', '#ff7800', '#055052', '#C400FF'],
+      borderWidth: 1,
+    },
+  ],
+};
 function Chart() {
-  return <div></div>;
+  return (
+    <div className={styles.chart}>
+      <div className='header'>
+        <h2 className='title'>Complaints Types Chart:</h2>
+        <div className='links'></div>
+      </div>
+      <Doughnut data={data} />
+    </div>
+  );
 }
 
 export default Chart;
