b3cd7ec6d959a0e3868909f284c49aa93d827f8f
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-11-02 13:43:42 +0200

    complaint cards done

--- src/components/customer/CustomerProfile.jsx
+++ src/components/customer/CustomerProfile.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
-
+import ComplaintCard from './ComplaintCard';
+import styles from '../../styles/customerProfile.module.css';
+import ComplaintForm from '../complaintForm/ComplaintForm';
 function CustomerProfile() {
-  return <div></div>;
+  return (
+    <div className={styles.bigContainer}>
+      <div className={styles.cardsContainer}>
+        <ComplaintCard />
+        <ComplaintCard />
+        <ComplaintCard />
+        <ComplaintCard />
+      </div>
+      <div className={styles.complaintForm}>
+        <ComplaintForm />
+      </div>
+    </div>
+  );
 }
 
 export default CustomerProfile;
