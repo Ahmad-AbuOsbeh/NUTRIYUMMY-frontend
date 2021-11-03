1dc0ae036626a5e94b5606f244416ee630a6f7c7
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-11-03 12:07:19 +0200

    finished user routes, create/get/delete

--- src/components/complaintForm/ComplaintForm.jsx
+++ src/components/complaintForm/ComplaintForm.jsx
@@ -1,20 +1,47 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import styles from '../../styles/complaintForm.module.css';
-function ComplaintForm() {
+import { instance, url } from '../../API/axios';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+function ComplaintForm({ fetchMyComplaint }) {
+  const history = useHistory();
+
+  const [complaintData, setComplaintData] = useState({});
+  const { isLoggedIn, user } = useSelector((state) => state.authReducer);
+
+  // addComplaintHandler
+  async function addComplaintHandler(e) {
+    e.preventDefault();
+    const response = await axios.post(`${url}/complaint`, complaintData, {
+      headers: {
+        authorization: `Bearer ${user?.token}`,
+      },
+    });
+    e.target.reset();
+    setComplaintData({});
+    fetchMyComplaint();
+  }
+
+  // on change handler
+  function onChangeHandler(e) {
+    setComplaintData({ ...complaintData, [e.target.name]: e.target.value });
+  }
   return (
     <div className={styles.formContainer}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={addComplaintHandler}>
         <h2>Complaint Form:</h2>
         <div className={styles.fieldsContainer}>
-          <select className={styles.field} placeholder='complaint type'>
+          <select className={styles.field} placeholder='complaint type' name='complaintType' onChange={onChangeHandler}>
             <option>Serial Complaints</option>
             <option>Personal Complaints</option>
             <option>Delivery Complaints</option>
             <option>Quality Complaints</option>
           </select>
-          <input className={styles.field} type='date' placeholder='date' />
-          <input className={styles.field} type='number' placeholder='Invoice number' />
-          <input className={`${styles.field} ${styles.fieldContent}`} type='text' placeholder='complaint content..' />
+          <input className={styles.field} type='date' placeholder='date' name='complaintDate' onChange={onChangeHandler} />
+          <input className={styles.field} type='number' placeholder='Invoice number' name='invoiceNumber' onChange={onChangeHandler} />
+          <input className={`${styles.field} ${styles.fieldContent}`} type='text' placeholder='complaint content..' name='complaintContent' onChange={onChangeHandler} />
         </div>
         <button className={styles.formbtn}>Submit</button>
       </form>
--- src/components/signIn/SignIn.jsx
+++ src/components/signIn/SignIn.jsx
@@ -20,6 +20,7 @@ function SignIn() {
       // decode the token to get the user info
       const user = jwt.decode(token);
       console.log(token, user);
+      user.token = token;
       // check if the user exists or not
       if (user) {
         // if he is a valid user, make him logged in
