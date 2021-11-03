dc8cce096e9b2cb959bd288404865feac54ab10c
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-11-03 05:18:22 +0200

    sign-in, sign-up form styling done

--- src/components/signIn/SignIn.jsx
+++ src/components/signIn/SignIn.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
-
+import styles from '../../styles/login.module.css';
 function SignIn() {
-  return <div></div>;
+  return (
+    <div className={styles.bigContainer}>
+      <form className={styles.form}>
+        <fieldset className={styles.fieldset}>
+          <legend>Login :</legend>
+
+          <label>Email: </label>
+          <input className={styles.field} type='email' placeholder='email' />
+          <label>Password: </label>
+          <input className={styles.field} type='password' placeholder='password' />
+
+          <button className={styles.button}>Login</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 }
 
 export default SignIn;
--- src/components/signUp/SignUp.jsx
+++ src/components/signUp/SignUp.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
-
+import styles from '../../styles/signup.module.css';
 function SignUp() {
-  return <div></div>;
+  return (
+    <div className={styles.bigContainer}>
+      <form className={styles.form}>
+        <fieldset className={styles.fieldset}>
+          <legend>Sign-Up Form :</legend>
+          <label>username: </label>
+          <input className={styles.field} type='text' placeholder='your name' />
+          <label>Email: </label>
+          <input className={styles.field} type='email' placeholder='email' />
+          <label>Password: </label>
+          <input className={styles.field} type='password' placeholder='password' />
+          <label>City: </label>
+          <select className={styles.field} placeholder='City'>
+            <option>Amman</option>
+            <option>Zarqa</option>
+            <option>Aqaba</option>
+            <option>Irbid</option>
+            <option>Ma'an</option>
+            <option>Mafraq</option>
+            <option>Salt</option>
+            <option>Ajloun</option>
+            <option>Jerash</option>
+            <option>Al-Karak</option>
+            <option>Tafielah</option>
+            <option>Madaba</option>
+          </select>
+          <label>role: </label>
+          <select className={styles.field} placeholder='role'>
+            <option>Customer</option>
+            <option>Admin</option>
+          </select>
+          <button className={styles.button}>Submit</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
